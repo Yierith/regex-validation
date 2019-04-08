b788ccd5e566a854ae2793201814be53de2ca5e5
Tim Rach <tim.rach1987@gmail.com>
2019-04-08 11:55:55 +0200

    Redesigned the phone regex

--- js/app.js
+++ js/app.js
@@ -23,13 +23,13 @@ function isValidPassword(password) {
 
 // The telephone number must be in the format of (555) 555-5555
 function isValidTelephone(telephone) {
-  return /^\(\d{3}\)\s\d{3}-\d{4}$/.test(telephone);
+  return /^\D*\d{3}\D*\d{3}\D*\d{4}$/.test(telephone);
 
 }
 
 // Must be a valid email address
 function isValidEmail(email) {
-
+  return /^[^@]+@[^@.]+\.[a-z]+$/.test(email);
 
 }
 
